5bb2b4a27745d9dab1c3533d81754fa929fb2309
garyguoit <51580332+garyguoit@users.noreply.github.com>
2020-09-14 18:14:09 -0700

    commit

--- MyWebApp/MyWebApp.DataAccess.SQL/DataContext.cs
+++ MyWebApp/MyWebApp.DataAccess.SQL/DataContext.cs
@@ -4,11 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.Entity;
+using MyWebApp.Core.Models;
 
 namespace MyWebApp.DataAccess.SQL
 {
     class DataContext : DbContext
     {
+        public DataContext()
+            :base("DefaultConnection")
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
 
     }
 }
