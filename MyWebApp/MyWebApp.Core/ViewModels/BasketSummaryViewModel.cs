b5f2233fe723813c3d4c341f55897ef8e279782b
garyguoit <51580332+garyguoit@users.noreply.github.com>
2020-09-20 14:55:10 -0700

    Basket

---
+++ MyWebApp/MyWebApp.Core/ViewModels/BasketSummaryViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyWebApp.Core.ViewModels
+{
+    public class BasketSummaryViewModel
+    {
+        public int BasketCount { get; set; }
+        public decimal BasketTotal { get; set; }
+
+        public BasketSummaryViewModel()
+        {
+
+        }
+
+        public BasketSummaryViewModel(int basketCount, decimal basket)
+        {
+            this.BasketCount = basketCount;
+            this.BasketTotal = BasketTotal;
+        }
+    }
+}
